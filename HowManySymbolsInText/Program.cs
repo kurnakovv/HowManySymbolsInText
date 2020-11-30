9a959788d274eff8fee468ccbf82fe1a75ddfcc4
KurnakovMaksim <maksk210@gmail.com>
2020-11-30 14:47:51 +0900

    Add project files.

---
+++ HowManySymbolsInText/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HowManySymbolsInText
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.Write("input string:");
+                string inputText = Console.ReadLine();
+                Console.WriteLine(inputText.Length);
+                Console.ReadKey();
+                Console.Clear();
+            }
+        }
+    }
+}
